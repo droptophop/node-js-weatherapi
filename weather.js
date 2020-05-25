9873de583c89b3bd58b9af2cf5dafaaa3e8499d5
Harold Hopson Jr <haroldh@fb.com>
2020-05-25 14:17:50 -0700

    Updated error handling in weather.js

--- weather.js
+++ weather.js
@@ -16,46 +16,56 @@ function printError(error) {
 }
 
 function getWeatherFor(query) {
-    const request = https.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${query})`, res => {
-        let body = '';
-        // console.log(API_KEY)
-    
-        if(res.statusCode == 200) {
-            res.on('data', data => {
-                body += data.toString();
-                // console.log(body);
-            })
-    
-            res.on('end', () => {
-                try{
-                    const weatherInfo = JSON.parse(body);
-                    const { 
-                        name,
-                        region,
-                        timestamp,
-                        temp,
-                        conditions
-                    } = { 
-                        name: weatherInfo.location.name,
-                        region: weatherInfo.location.region,
-                        timestamp: weatherInfo.current.last_updated,
-                        temp: weatherInfo.current.temp_f,
-                        conditions: weatherInfo.current.condition.text
+    try {
+        const request = https.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${query})`, res => {
+            let body = '';
+            // console.log(API_KEY)
+        
+            if(res.statusCode == 200) {
+                res.on('data', data => {
+                    body += data.toString();
+                    // console.log(body);
+                })
+        
+                res.on('end', () => {
+                    try{
+                        const weatherInfo = JSON.parse(body);
+                        if(weatherInfo.location) {
+                            console.log(weather.location)
+                            const { 
+                                name,
+                                region,
+                                timestamp,
+                                temp,
+                                conditions
+                            } = { 
+                                name: weatherInfo.location.name,
+                                region: weatherInfo.location.region,
+                                timestamp: weatherInfo.current.last_updated,
+                                temp: weatherInfo.current.temp_f,
+                                conditions: weatherInfo.current.condition.text
+                            }
+                            dashboard(name, region, timestamp, temp, conditions)
+                            // console.log(conditions);
+                        } else {
+                            const queryError = new Error(`The location "${weather.location}" was not found.`)
+                            printError(queryError)
+                        }
+                    } catch(e) {
+                        printError(e)
                     }
-                    dashboard(name, region, timestamp, temp, conditions)
-                    // console.log(conditions);
-                } catch(e) {
-                    printError(e)
-                }
-            })
-    
-    
-        } else {
-            const message = `There was an error retrieving the weather details for ${query} (${http.STATUS_CODES[res.statusCode]})`
-            const statusCodeError = new Error(message)
-            printError(statusCodeError)
-        }
-    })
+                })
+        
+        
+            } else {
+                const message = `There was an error retrieving the weather details for ${query} (${http.STATUS_CODES[res.statusCode]})`
+                const statusCodeError = new Error(message)
+                printError(statusCodeError)
+            }
+        })
+    } catch(e) {
+        printError(e)
+    }
 }
 
 module.exports.getWeatherFor = getWeatherFor
